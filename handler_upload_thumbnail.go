95cbf183c30c0960879869c8cf43413ab5fe591b
mharkness1 <mharkness.uk@gmail.com>
2025-05-11 21:21:53 +0100

    A: WIP updating thumbnail system. Need to do: AWS.

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +35,78 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory int = 10 << 20
+	err = r.ParseMultipartForm(int64(maxMemory))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse form", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get file", err)
+		return
+	}
+	defer file.Close()
+
+	headerDetails := header.Header.Get("Content-Type")
+	mediaType, _, err := mime.ParseMediaType(headerDetails)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to process file", err)
+		return
+	}
+
+	contentTypeToExt := map[string]string{
+		"image/png":  ".png",
+		"image/jpeg": ".jpg",
+	}
+	extension, ok := contentTypeToExt[mediaType]
+	if !ok {
+		respondWithError(w, http.StatusBadRequest, "Unsupported file type", nil)
+		return
+	}
+
+	databaseVideo, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get video", err)
+		return
+	}
+	if databaseVideo.UserID != userID {
+		respondWithError(w, http.StatusForbidden, "You don't own this video", err)
+		return
+	}
+
+	key := make([]byte, 32)
+	rand.Read(key)
+	keyEncoding := base64.RawURLEncoding.EncodeToString(key)
+
+	fileName := fmt.Sprintf("%s%s", keyEncoding, extension)
+	filePath := filepath.Join(cfg.assetsRoot, fileName)
+	newFile, err := os.Create(filePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to create file", err)
+		return
+	}
+	defer newFile.Close()
+
+	_, err = io.Copy(newFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to save file", err)
+		return
+	}
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, fileName)
+	updatedVideo := databaseVideo
+	updatedVideo.ThumbnailURL = &thumbnailURL
+	updatedVideo.UpdatedAt = time.Now()
+
+	err = cfg.db.UpdateVideo(updatedVideo)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, updatedVideo)
 }
